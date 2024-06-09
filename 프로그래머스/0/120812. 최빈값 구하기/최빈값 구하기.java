658ee4d7bf33d8cc46f8da50db4ae15976c2e3f3
Kobe <124736003+devKobe24@users.noreply.github.com>
2024-06-09 09:08:04 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.05 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,21 @@
+public class Solution {
+	public int solution(int[] array) {
+		int answer = 0;
+		int[] index = new int[1000];
+		int max = Integer.MIN_VALUE;
+
+		for (int i = 0; i < array.length; i++) {
+			index[array[i]]++;
+		}
+
+		for (int i = 0; i < index.length; i++) {
+			if (max < index[i]) {
+				max = index[i];
+				answer = i;
+			} else if (max == index[i]) {
+				answer = -1;
+			}
+		}
+		return answer;
+	}
+}
